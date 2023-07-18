67b8d003d3d75ff0dfb78239fbd99c76828b29fa
mdfultz <129699601+mdfultz@users.noreply.github.com>
2023-07-17 20:27:20 -0400

    complete

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
 # add your code here
 
+x=1
+
+while x <= 100:
+    if x % 3 == 0:
+        if x % 5 == 0:
+            print('FizzBuzz')
+        else:
+            print('Fizz')
+    else:
+        print (x)
+    x +=1
